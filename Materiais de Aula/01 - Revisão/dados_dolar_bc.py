cc9ae3a6fb49bcbcf7f7ec1e581e8f5e9fc7dc92
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2023-04-03 21:16:15 -0300

    Add files via upload

---
+++ Materiais de Aula/01 - Revisão/dados_dolar_bc.py
@@ -0,0 +1,2 @@
+url  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%2701-01-2021%27&@dataFinalCotacao=%2712-31-2021%27&$format=json'
+
---
+++ Materiais de Aula/01 - Revisão/dados_ifrn.py
@@ -0,0 +1,2 @@
+url = 'https://dados.ifrn.edu.br/dataset/d5adda48-f65b-4ef8-9996-1ee2c445e7c0/resource/00efe66e-3615-4d87-8706-f68d52d801d7/download/dados_extraidos_recursos_alunos-da-instituicao.json'
+
