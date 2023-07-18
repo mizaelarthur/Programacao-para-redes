ce3c5760cfb9f79e082bf919fc9575766940cb69
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2023-07-18 14:26:21 +0000

    Implementando Servidor

--- ProjetoProgRedes/servidor.py
+++ ProjetoProgRedes/servidor.py
@@ -4,4 +4,15 @@ import socket, threading
 SERVER = '0.0.0.0'
 PORT = 5678
 
+# Criei uma função para interação de mensagem. Caso o cliente digite "/q" ele encerra o socket.
+def cliInteraction(sockConn, addr):
+    msg = b''
+    while msg != b'/q':
+        try:
+            msg = sockConn.recv(512)
+            broadCast (msg, addr)
+        except:
+            msg = b'/q'
+    allSocks.remove ((sockConn, addr))
+    sockConn.close()
 
