c3046313e5f2e7a9fbd76c5f7b2eb2422491b8e5
M1Z43L <33938612+mizaelarthur@users.noreply.github.com>
2023-07-20 09:26:53 -0300

    Akterações

--- ProjetoProgRedes/servidorV2.py
+++ ProjetoProgRedes/servidorV2.py
@@ -1,26 +1,62 @@
 import socket
+import threading
 
 # Define o host e porta do servidor
 HOST = '0.0.0.0'
 PORTA = 12345
 
-def mensagem(client_socket, msg):
-    client_socket.send(msg.encode('utf-8'))
-    data = client_socket.recv(1024).decode('utf-8')
-    print(data)
+# Dicionário para armazenar as mensagens enviadas pelos clientes
+mensagens_enviadas = {}
 
-def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORTA))
-
+def handle_client(client_socket):
     while True:
-        msg = input('Digite um comando: ')
-        mensagem(client_socket, msg)
-
-        if msg == '/q':
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            
+            # Separa o comando e os parâmetros da mensagem
+            comando, *parametros = data.strip().split(':')
+            
+            # Implementação dos comandos
+            if comando == '/q':
+                client_socket.send('Saindo...'.encode('utf-8'))
+                break
+            
+            elif comando == '/l':
+                client_socket.send(str(mensagens_enviadas).encode('utf-8'))
+            
+            elif comando == '/m':
+                ip_destino, porta_destino, mensagem = parametros
+                key = f'{ip_destino}:{porta_destino}'
+                if key in mensagens_enviadas:
+                    mensagens_enviadas[key].append(mensagem)
+                else:
+                    mensagens_enviadas[key] = [mensagem]
+                client_socket.send('Mensagem enviada com sucesso.'.encode('utf-8'))
+            
+            # Implemente os outros comandos aqui
+            
+            else:
+                client_socket.send('Comando inválido.'.encode('utf-8'))
+        
+        except Exception as e:
+            print(f'Erro: {e}')
             break
 
     client_socket.close()
 
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORTA))
+    server.listen(5)
+    print(f'Servidor escutando em {HOST}:{PORTA}')
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f'Conexão estabelecida com {addr[0]}:{addr[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
+
 if __name__ == '__main__':
-    main()
+    start_server()
