53cb52dfbfbb07b28225185428731b4f74b23d49
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-30 16:46:31 -0300

    update

--- ProjetoProgRedes/servidorV1.py
+++ ProjetoProgRedes/servidorV1.py
@@ -1,39 +1,11 @@
-import socket, threading
+'''Realizando a importação de bibliotecas Python
+   para nos ajudar com as demandas '''
 
-SERVER = '0.0.0.0'
-PORT = 5678
+import socket
+import threading
+import feedparser
+import requests
+import os
 
-def cliInteraction(sockConn, addr):
-    msg = b''
-    while msg != b'/q':
-        try:
-            msg = sockConn.recv(512)
-            broadCast (msg, addr)
-        except:
-            msg = b'/q'
-    allSocks.remove ((sockConn, addr))
-    sockConn.close()
-
-def broadCast(msg, addrSource):
-    msg = f"{addrSource} -> {msg.decode('utf-8')}"
-    print (msg)
-    for sockConn, addr in allSocks:
-        if addr != addrSource:
-            sockConn.send(msg.encode('utf-8'))
-
-try:
-    allSocks = []
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((SERVER, PORT))
-
-    print ("Listening in: ", (SERVER, PORT))
-    sock.listen(5)
-
-    while True:
-        sockConn, addr = sock.accept()
-        print ("Connection from: ", addr)
-        allSocks.append((sockConn, addr))
-        tClient = threading.Thread(target=cliInteraction, args=(sockConn, addr))
-        tClient.start()
-except Exception as e:
-    print ("Fail: ", e)
+# Agora vamos importas os outros arquivos de Configuração
+import rss_config
