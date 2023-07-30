62af068f6abbf71375aaac7aae90a1324321aa02
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-30 09:59:12 -0300

    Update

--- ProjetoProgRedes/cliente3.py
+++ ProjetoProgRedes/cliente3.py
@@ -1,16 +1,28 @@
 import socket
 
-def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 8888))
+server_ip = '127.0.0.1'  # Altere para o IP do servidor
+server_port = 12345  # Altere para a porta do servidor
 
+# Função para receber mensagens do servidor
+def receive_message(client_socket):
     while True:
-        command = input("Digite um comando (/q para sair): ")
-        s.sendall(command.encode('utf-8'))
-        data = s.recv(1024).decode('utf-8')
+        data = client_socket.recv(1024).decode('utf-8')
         print(data)
 
-    s.close()
+# Configuração do socket do cliente
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, server_port))
 
-if __name__ == "__main__":
-    main()
+# Iniciando thread para receber mensagens do servidor
+receive_thread = threading.Thread(target=receive_message, args=(client,))
+receive_thread.start()
+
+# Loop principal para enviar comandos ao servidor
+while True:
+    command = input()
+    client.send(command.encode('utf-8'))
+
+    if command == "/q":
+        print("Desconectando...")
+        client.close()
+        break
