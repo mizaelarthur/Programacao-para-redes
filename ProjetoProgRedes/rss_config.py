1292c1e568542aa400951f8ebdb8e5ce93d018a9
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-30 16:46:28 -0300

    update

---
+++ ProjetoProgRedes/rss_config.py
@@ -0,0 +1,14 @@
+'''Esse arquivo contem as fontes RSS apenas'''
+
+RSS_FEEDS = [
+    "https://g1.globo.com/dynamo/rss2.xml",
+    "http://www.bbc.co.uk/portuguese/index.xml",
+    "http://www.bbc.co.uk/portuguese/topicos/brasil/index.xml",
+    "http://www.bbc.co.uk/portuguese/topicos/america_latina/index.xml",
+    "http://www.bbc.co.uk/portuguese/topicos/internacional/index.xml",
+    "http://www.bbc.co.uk/portuguese/topicos/economia/",
+    "http://www.bbc.co.uk/portuguese/topicos/saude/",
+    "http://www.bbc.co.uk/portuguese/topicos/ciencia_e_tecnologia/",
+    "http://www.bbc.co.uk/portuguese/topicos/cultura/",
+    "http://www.bbc.co.uk/portuguese/especiais/index.xml"
+]
