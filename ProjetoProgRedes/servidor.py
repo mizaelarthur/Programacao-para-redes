82bd87f7e8f20b231e906fe9030b0d3abe1e9915
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-31 00:02:38 -0300

    update

--- ProjetoProgRedes/servidor.py
+++ ProjetoProgRedes/servidor.py
@@ -8,9 +8,30 @@ import requests
 import os
 
 # Agora vamos importas os outros arquivos de Configuração
-import rss_config
-import telegram
+from comunicação_cliente import  comunicacao_cliente
+from log import LOG_FILE
 
+
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 8888))
+    server_socket.listen(5)
+    print("Servidor iniciado. Aguardando conexões...")
+
+    while True:
+        client_socket, address = server_socket.accept()
+        print(f"Cliente {address[0]}:{address[1]} conectado.")
+        clients[address] = client_socket
+        client_handler = threading.Thread(target=comunicacao_cliente, args=(client_socket, address))
+        client_handler.start()
+
+if __name__ == "__main__":
+    clients = {}
+
+    # Criar arquivo de log
+    open(LOG_FILE, "w").close()
+
+    start_server()
 
 
 
