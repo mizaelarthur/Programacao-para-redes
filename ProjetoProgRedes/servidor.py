e7c15d969caab53eb1ea0f0f873670fd4a2ee800
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-30 23:44:49 -0300

    update

---
+++ ProjetoProgRedes/servidor.py
@@ -0,0 +1,13 @@
+'''Realizando a importação de bibliotecas Python
+   para nos ajudar com as demandas '''
+
+import socket
+import threading
+import feedparser
+import requests
+import os
+
+# Agora vamos importas os outros arquivos de Configuração
+import rss_config
+
+
